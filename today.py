cd6efee88641990e7456f4daab27e74452a48c80
yousuf119 <mohamedyousuf1136@gmail>
2021-04-15 00:47:10 +0530

    file modifies

--- today.py
+++ today.py
@@ -2,12 +2,12 @@ from git import Repo
 
 PATH_OF_GIT_REPO = r'C:\Users\mohamed yousuf\Desktop\gitpractice\gitpractice'
  # make sure .git folder is properly configured
-COMMIT_MESSAGE = 'comment from python script today.py'
+COMMIT_MESSAGE = 'newyork nagaram pushed successfully'
 
 def git_push():
     try:
         repo = Repo(PATH_OF_GIT_REPO)
-        repo.git.add(update=True)
+        repo.git.add(newyork.txt)
         repo.index.commit(COMMIT_MESSAGE)
         origin = repo.remote(name='origin')
         origin.push()
